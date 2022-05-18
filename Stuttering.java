d0606831ec9aeb469bf069066e3b132e0848c254
justinetu <71947079+justinetu@users.noreply.github.com>
2022-05-18 12:31:10 -0500

    Update Stuttering.java

--- Stuttering.java
+++ Stuttering.java
@@ -2,7 +2,9 @@ public class Stuttering {
 
 	/*
 	Question:
-	Write a function that stutters a word as if someone is struggling to read it. The first two letters are repeated twice with an ellipsis ... and space after each, and then the 		word is pronounced with a question mark ?.
+	Write a function that stutters a word as if someone is struggling to read it. 
+	The first two letters are repeated twice with an ellipsis ... and space after
+	each, and then the word is pronounced with a question mark?.
 
 	Examples
 	stutter("incredible") ➞ "in... in... incredible?"
