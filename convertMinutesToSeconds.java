f40f8c76f32992fbb36a33cbd693f823e534d3f4
justinetu <71947079+justinetu@users.noreply.github.com>
2022-05-18 12:24:37 -0500

    Add files via upload

---
+++ convertMinutesToSeconds.java
@@ -0,0 +1,19 @@
+/* Question
+   Write a function that takes an integer minutes and converts it to seconds.
+
+	Examples:
+	convert(5) ➞ 300
+
+	convert(3) ➞ 180
+
+	convert(2) ➞ 120
+*/
+
+// Solution
+
+public class Challenge {
+	public static int convert(int minutes) {
+		int seconds = 60 * minutes;
+		return seconds;
+  }
+}
