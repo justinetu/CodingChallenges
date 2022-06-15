519b7440f60823382b4a84361fae69552e88a93d
Justin Etu <71947079+justinetu@users.noreply.github.com>
2022-06-15 01:03:41 -0500

    Add files via upload

---
+++ perimeterOfRectangle.java
@@ -0,0 +1,19 @@
+/*
+	Question
+	Create a function that takes length and width and finds the perimeter of a rectangle.
+
+	Examples
+	findPerimeter(6, 7) ➞ 26
+
+	findPerimeter(20, 10) ➞ 60
+
+	findPerimeter(2, 9) ➞ 22
+*/
+
+//Code
+
+public class perimeterOfRectangle {
+	public static int findPerimeter(int length, int width) {
+		return length * 2 + width * 2;
+  }
+}
