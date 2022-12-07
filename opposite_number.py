977e78291d9310d901dd83617bab583d26ce46f1
Justin Etu <71947079+justinetu@users.noreply.github.com>
2022-12-07 00:28:34 -0600

    Create opposite_number.py

---
+++ opposite_number.py
@@ -0,0 +1,12 @@
+#Very simple, given an integer or a floating-point number, find its opposite.
+
+#Examples:
+
+#1: -1
+#14: -14
+
+def opposite(number):
+  # your solution here
+    if number > 0: return -number
+
+    return abs(number)
